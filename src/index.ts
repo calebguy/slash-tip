e5cb5657a787446dbd8b67058c6c591dd068f8f5
caleb <caleb@syndicate.io>
2024-05-01 00:38:55 -0500

    ♒️

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,9 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.post('/tip', (c) => {
+  console.log('got body', c.body)
+  return c.json({ message: 'tipped' })
+})
+
 export default app
