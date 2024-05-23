847c432a0eff26370526c602c077628988fbb4c4
caleb <caleb@syndicate.io>
2024-05-23 10:30:33 -0500

    mobile layout

--- server/ui/src/App.tsx
+++ server/ui/src/App.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query"
 import { hc, type InferResponseType } from "hono/client"
+import { useCallback } from "react"
 import type { ApiType } from "../../src/index"
 import "./App.css"
 import { abbreviate } from "./utils"
@@ -23,55 +24,57 @@ function App() {
 		queryFn: getLeaderboard,
 	})
 
+	const renderUser = useCallback((user: any) => {
+		const [first, last] = abbreviate(user.account)
+		return (
+			<div
+				key={`user-${user.nickname}`}
+				className="group inline-block"
+				onClick={() => navigator.clipboard.writeText(user.account)}
+				onKeyDown={() => navigator.clipboard.writeText(user.account)}
+				onDoubleClick={() =>
+					window.open(`https://basescan.org/address/${user.account}`)
+				}
+			>
+				<div className="inline-block group-hover:hidden">
+					{user.nickname}
+				</div>
+				<div className="hidden group-hover:block cursor-pointer active:text-[#02d100] select-none group">
+					<span>{first}</span>
+					<span className="inline-block align-bottom leading-10 text-6xl ml-2">
+						***
+					</span>
+					<span>{last}</span>
+				</div>
+			</div>
+		)
+	}, [])
+
 	return (
-		<div className="font-jacquard text-8xl flex-grow flex flex-col">
-			<div>*/tip</div>
-			<div className="grid grid-cols-2 grow py-8">
-				<div className="text-right">
+		<div className="font-jacquard text-6xl md:text-8xl flex-grow flex flex-col">
+			<div className="text-left md:text-center">*/tip</div>
+			<div className="md:grid md:grid-cols-2 grow py-8">
+				<div className="text-left md:text-right">
 					{data?.map((user) => (
 						<div key={`user-${user.nickname}-${user.balance}`}>
-							{user.balance}*/
+							{user.balance}*/<span className="inline-block md:hidden">{renderUser(user)}</span>
 						</div>
 					))}
 				</div>
-				<div className="text-left flex flex-col items-start">
-					{data?.map((user) => {
-						const [first, last] = abbreviate(user.account)
-						return (
-							<div
-								key={`user-${user.nickname}`}
-								className="group inline-block"
-								onClick={() => navigator.clipboard.writeText(user.account)}
-								onKeyDown={() => navigator.clipboard.writeText(user.account)}
-								onDoubleClick={() =>
-									window.open(`https://basescan.org/address/${user.account}`)
-								}
-							>
-								<div className="inline-block group-hover:hidden">
-									{user.nickname}
-								</div>
-								<div className="hidden group-hover:block cursor-pointer active:text-[#02d100] select-none group">
-									<span>{first}</span>
-									<span className="inline-block align-bottom leading-10 text-6xl ml-2">
-										***
-									</span>
-									<span>{last}</span>
-								</div>
-							</div>
-						)
-					})}
+				<div className="hidden md:flex text-left flex-col items-start">
+					{data?.map(renderUser)}
 				</div>
 			</div>
-			<div className="flex flex-col justify-end items-center gap-y-8 py-12">
+			<div className="flex flex-col justify-end items-center gap-y-8 py-8 md:py-12">
 				<button
 					type="button"
-					className="text-6xl p-2 bg-[#02d100] text-[#03ff00] cursor-not-allowed"
+					className="text-4xl md:text-6xl p-2 bg-[#02d100] text-[#03ff00] cursor-not-allowed"
 				>
 					redeem
 				</button>
 			</div>
 			<div className="flex justify-center">
-				<img src={"/black-gif.gif"} alt="✺" width={35} height={35} />
+				<img src={"/black-gif.gif"} alt="✺" width={45} height={45} />
 			</div>
 		</div>
 	)
