93768cb8c0deb3c9c16e89fa11eecc2176ee6064
caleb <caleb@syndicate.io>
2024-05-24 02:51:43 -0500

    up

--- server/src/cron-allowance.ts
+++ server/src/cron-allowance.ts
@@ -1,12 +1,17 @@
+import { addAllowanceForAllUsers } from "./chain";
+
 async function main() {
 	const allowance = 1;
 	const now = new Date();
 	console.log(
 		`[${now.toISOString()}] adding allowance of ${allowance} for all users`,
 	);
-	// await addAllowanceForAllUsers(allowance);
+	await addAllowanceForAllUsers(allowance);
 }
 
 main()
-	.then(() => process.exit(1))
-	.catch(console.error);
+	.then(() => process.exit(0))
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
+	});
