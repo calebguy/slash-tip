3bee96770171b99eff2fcc34ae30d2fae6ec0481
caleb <caleb@syndicate.io>
2024-05-24 01:19:22 -0500

    up

--- server/src/middleware.ts
+++ server/src/middleware.ts
@@ -1,10 +1,10 @@
 import { createMiddleware } from "hono/factory";
-import { getUserAddress } from "./chain";
+import { getUserExists } from "./chain";
 import type { SlackSlashCommandPayload } from "./types";
 
 export const mustBeRegistered = createMiddleware(async (c, next) => {
 	const { user_id } = await c.req.parseBody<SlackSlashCommandPayload>();
-	if (!(await getUserAddress(user_id))) {
+	if (!(await getUserExists(user_id))) {
 		return c.json({
 			response_type: "ephemeral",
 			text: "Register first with '/register <your-eth-address>'",
