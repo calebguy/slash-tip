b83a9d9138cba9c7c96a233a6fcae8e0a88a04ec
caleb <caleb@syndicate.io>
2025-03-14 18:11:43 +0100

    up

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import { type InferResponseType, hc } from "hono/client";
-import type { ApiType } from "../../src/server";
+import type { ApiType } from "../../server/src/server";
+import { IDK } from "./icons/idk";
+import { Logo } from "./icons/logo";
 import { abbreviate } from "./utils";
 
 const client = hc<ApiType>("/");
@@ -26,12 +28,15 @@ function App() {
 
 	return (
 		<div className="font-jacquard text-4xl md:text-6xl grow flex flex-col justify-stretch items-stretch h-full">
-			<div className="text-left md:text-center">/tip</div>
+			<div className="flex items-center">
+				<Logo className="w-14 h-14 text-slime" />
+				<div className="font-thin text-5xl text-slime">/tip</div>
+			</div>
 			<div className="md:grid md:grid-cols-2 grow overflow-auto my-8">
-				<div className="text-left md:text-right font-thin">
+				<div className="text-left md:text-right font-thin text-slime">
 					{data?.map((user) => (
 						<div key={`user-${user.nickname}-${user.balance}`}>
-							<span className="font-thin">{user.balance}</span>/
+							<span className="font-thin text-slime">{user.balance}</span>/
 							<span className="inline-block md:hidden font-light">
 								<User user={user} />
 							</span>
@@ -42,21 +47,6 @@ function App() {
 					{data?.map((user) => (
 						<User key={`user-${user.id}`} user={user} />
 					))}
-				</div>
-			</div>
-			<div>
-				<div className="flex flex-col justify-end items-center gap-y-8">
-					<button
-						type="button"
-						className="rounded-3xl px-4 text-4xl p-2 bg-[#02d100] text-[#03ff00] cursor-not-allowed active:translate-x-px active:translate-y-px"
-					>
-						redeem
-					</button>
-				</div>
-				<div className="flex justify-center mt-6">
-					<span className="w-12 h-12">
-						<img src={"/logo.svg"} alt="syn" />
-					</span>
 				</div>
 			</div>
 		</div>
@@ -75,11 +65,15 @@ const User = ({ user }: { user: User }) => {
 				window.open(`https://basescan.org/address/${user.account}`)
 			}
 		>
-			<div className="inline-block group-hover:hidden">{user.nickname}</div>
-			<div className="hidden group-hover:block cursor-pointer active:text-[#02d100] select-none group">
+			<div className="inline-block group-hover:hidden text-paper">
+				{user.nickname}
+			</div>
+			<div className="group hidden group-hover:block cursor-pointer text-orange active:text-orange/70 select-none">
 				<span>{first}</span>
-				<span className="inline-flex items-center mx-1">
-					<img src={"/thing.svg"} alt="syn" className="w-6 h-6" />
+				<span className="inline-flex items-center gap-0.5 md:gap-1">
+					<IDK className="md:w-5 md:h-5 w-4 h-4" />
+					<IDK className="md:w-5 md:h-5 w-4 h-4" />
+					<IDK className="md:w-5 md:h-5 w-4 h-4" />
 				</span>
 				<span>{last}</span>
 			</div>
