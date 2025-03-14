5a278a8c58e5b51df03b62e7b81e05168eb12fbe
caleb <caleb@syndicate.io>
2025-03-14 18:33:00 +0100

    space

--- packages/ui/src/components/Header.tsx
+++ packages/ui/src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Logo } from "../icons/logo";
 
 export function Header() {
 	return (
-		<div className="flex justify-start">
+		<div className="flex justify-start mb-4">
 			<Link to="/" className="inline-flex items-center">
 				<Logo className="md:w-14 md:h-14 w-10 h-10 text-slime" />
 				<div className="font-thin text-3xl md:text-5xl text-slime">/tip</div>
