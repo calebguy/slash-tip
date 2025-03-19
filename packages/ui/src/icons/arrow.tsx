f011a958e3071749e9166b74f32bb582e4c265d2
caleb <caleb@syndicate.io>
2025-03-19 09:44:08 +0100

    fix

--- packages/ui/src/icons/arrow.tsx
+++ packages/ui/src/icons/arrow.tsx
@@ -12,9 +12,9 @@ export function Arrow({ title, ...props }: SvgProps) {
 			<path
 				d="M6 1L6 12"
 				stroke="currentColor"
-				stroke-width="0.5"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="0.5"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 			<path
 				d="M6.11408 11.844L10.0014 7.6608L10.1806 7.82987L5.74692 12.6057L1.2954 7.83404L1.48959 7.6558L5.38154 11.844L5.74781 12.2381L6.11408 11.844Z"
