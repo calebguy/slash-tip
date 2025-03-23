8e6cb7921c1629b9f3d629b9094265dd65728d90
caleb <caleb@syndicate.io>
2025-03-24 00:37:13 +0100

    try new abi

--- packages/ponder/src/index.ts
+++ packages/ponder/src/index.ts
@@ -1,4 +1,5 @@
 import { ponder } from "ponder:registry";
+import SlashTipAbi from "utils/src/abis/SlashTipAbi";
 import { decodeFunctionData, zeroAddress } from "viem";
 import { OldSlashTipAbi } from "./OldSlashTipAbi";
 import { db, publicClient } from "./shared";
@@ -36,7 +37,38 @@ ponder.on("Tip:TransferSingle", async ({ event, context }) => {
 			});
 		}
 	} catch (e) {
-		console.warn("skipping...");
+		console.warn("old abi failed, trying new abi...");
 		console.error(e);
+
+		try {
+			const {
+				args: [fromUserId, toUserId, tokenId, amount],
+			} = decodeFunctionData({
+				abi: SlashTipAbi,
+				data: tx.input,
+			});
+			if (
+				fromUserId !== zeroAddress &&
+				toUserId !== zeroAddress &&
+				tokenId !== undefined &&
+				amount !== undefined
+			) {
+				const block = await publicClient.getBlock({
+					blockNumber: tx.blockNumber,
+				});
+				await db.upsertTip({
+					txHash: hash,
+					fromUserId: fromUserId as string,
+					toUserId: toUserId as string,
+					tokenId,
+					amount,
+					blockNumber: tx.blockNumber,
+					blockCreatedAt: new Date(Number(block.timestamp) * 1000),
+				});
+			}
+		} catch (e) {
+			console.warn("new abi failed, skipping...");
+			console.error(e);
+		}
 	}
 });
